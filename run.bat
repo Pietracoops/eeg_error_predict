db0464a19413712e6fbb64975982b7478fd46d1d
pietracoops <pietracoops@gmail.com>
2024-01-17 15:43:16 -0500

    adding environment variables to run script

--- run.bat
+++ run.bat
@@ -1,5 +1,12 @@
 @echo off
 
+rem Set environment variables
+echo Setting environment variables...
+set CUDA_PATH "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8" /M
+set PATH "%CUDA_PATH%\bin;%PATH%" /M
+set CUDA_LIB_PATH "%CUDA_PATH%\lib\x64" /M
+set PATH "%CUDA_LIB_PATH%;%PATH%" /M
+
 rem Set the path to the Python executable
 set python_executable=python
 
--- setup.bat
+++ setup.bat
@@ -115,7 +115,6 @@ rem Set environment variables
 echo Setting environment variables...
 set CUDA_PATH "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8" /M
 set PATH "%CUDA_PATH%\bin;%PATH%" /M
-set PATH "%CUDNN_HOME%\bin;%PATH%" /M
 set CUDA_LIB_PATH "%CUDA_PATH%\lib\x64" /M
 set PATH "%CUDA_LIB_PATH%;%PATH%" /M
 
